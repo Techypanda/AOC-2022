6c0c3d794854f82e3eaab92f2432429e87df24eb
techypanda <jonathan_wright@hotmail.com>
2022-12-03 02:12:48 +1100

    [FEAT] Frontend Challenge 2
    frontend is implemented for challenge 2
    
    jonwrigt@amazon.com

--- packages/web/src/components/Header.tsx
+++ packages/web/src/components/Header.tsx
@@ -6,6 +6,7 @@ export function Header() {
         <header className="pt-3 px-3">
             <h1 className='text-xl select-none cursor-pointer inline-block' onClick={() => navigate('/')}>AOC 2022 - Jonwrigt</h1>
             <h1 className='text-xs select-none cursor-pointer inline-block ml-4' onClick={() => navigate('/1')}>Challenge 1</h1>
+            <h1 className='text-xs select-none cursor-pointer inline-block ml-4' onClick={() => navigate('/2')}>Challenge 2</h1>
             <hr />
         </header>
     )
--- packages/web/src/features/day02/ChallengeTwoQueries.tsx
+++ packages/web/src/features/day02/ChallengeTwoQueries.tsx
@@ -1,23 +1,26 @@
 import { useMutation } from '@tanstack/react-query';
 import ky from 'ky';
 interface ExpectedResponse {
-    max: number[];
+    scores: number[];
     sum: number;
 }
 interface Payload {
     input: File;
-    topX: number;
+    v2: boolean;
 }
 const fetchAnswer = async (p: Payload) => {
     const formData = new FormData();
     formData.append('data', p.input);
-    return await ky(`${import.meta.env.VITE_API_URI}/1/${p.topX}`, {
+    if (p.v2) {
+        formData.append('v2', p.v2.toString());
+    }
+    return await ky(`${import.meta.env.VITE_API_URI}/2`, {
         method: 'post',
         body: formData
     }).json<ExpectedResponse>();
 };
 
-export const useChallengeOne = () => {
+export const useChallengeTwo = () => {
     const mutation = useMutation(fetchAnswer);
     return { mutation };
 }
